0e56268941d5afe8f370759230a93368cc75928f
안나 <dksek3050@gmail.com>
2025-03-20 23:59:29 +0900

    update

--- readmeUpdate.js
+++ readmeUpdate.js
@@ -103,7 +103,7 @@ const parser = new Parser({
 (async () => {
 
     // 피드 목록
-    const feed = await parser.parseURL('https://devpad.tistory.com/rss'); // 본인의 블로그 주소
+    const feed = await parser.parseURL('https://dksek3050.tistory.com/rss'); // 본인의 블로그 주소
 
     text += `<ul>`;
 
